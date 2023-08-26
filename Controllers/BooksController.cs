0d410ecc72134a922fa8d1d3ce97ca64d687d1e4
gabriel domingues <gabriel1977domingues@gmail.com>
2023-08-26 02:01:06 -0400

    Finalizado

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,78 @@
+﻿using book.Model;
+using book.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace book.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository) {
+            
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet("GetAll")]
+
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("GetById")]
+
+        public async Task<ActionResult<Book>> GetBooksForId(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost("Create")]
+
+        public async Task<ActionResult<Book>> CreateBook([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.create(book);
+            return book;
+        }
+
+        [HttpDelete("Delete")]
+
+        public async Task<ActionResult<Book>> Delete(int id)
+        {
+            var bookDelete = await _bookRepository.Get(id);
+
+            if(bookDelete == null) {
+                return NotFound();
+            }
+            else
+            {
+                await _bookRepository.Delete(bookDelete.id);
+
+            }
+
+            return NoContent();
+        }
+
+        private ActionResult<Book> NoContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ActionResult<Book> NotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("Update")]
+
+        public async Task<ActionResult> UpdateBooks(int id, [FromBody] Book book)
+        {
+            if(id == book.id)
+            {
+                await _bookRepository.Update(book);
+            }
+            return null;
+        }
+    }
+}
---
+++ Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace book.Model
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string Author { get; set; }
+        public string Descricao { get; set; }
+        public string Titulo { get; set; }
+    }
+}
